02b5bab05752c20fbd289e8450f2aebceb754fcc
Eddie <aditya.subedi@gmail.com>
2019-01-20 11:46:09 -0800

    Automated Page generation on vue.config.js

--- themes/gnosis/campagin-src/vue.config.js
+++ themes/gnosis/campagin-src/vue.config.js
@@ -1,22 +1,22 @@
+var pageName = ["amazon", "walmart"];
+var pages = {};
+for (let index in pageName) {
+  let page = pageName[index];
+  pages[page] = {
+    entry: `src/${page}/main.js`,
+    template: `src/${pageName[index]}/${page}.htm`,
+    filename: `${page}.htm`
+  };
+}
+
 module.exports = {
   outputDir: "../pages",
   assetsDir: "../assets",
   publicPath: "/themes/gnosis/assets",
-  pages: {
-    amazon: {
-      entry: "src/amazon/main.js",
-      template: "src/amazon/amazon.htm",
-      filename: "amazon.htm"
-    },
-    walmart: {
-      entry: "src/walmart/main.js",
-      template: "src/walmart/walmart.htm",
-      filename: "walmart.htm"
-    }
-  },
+  pages,
   chainWebpack: config => {
     config.plugins.delete("preload-search");
-    config.plugin("html-amazon").tap(args => {
+    config.plugin(`html-${pageName[0]}`).tap(args => {
       args[0].minify.collapseWhitespace = false;
       return args;
     });
@@ -26,3 +26,15 @@ module.exports = {
 // https://github.com/kangax/html-minifier#options-quick-reference
 // https://forum.vuejs.org/t/enable-minify-removeattributequotes/47431/6
 // npx vue-cli-service inspect --plugins
+// pages: {
+//   amazon: {
+//     entry: "src/amazon/main.js",
+//     template: "src/amazon/amazon.htm",
+//     filename: "amazon.htm"
+//   },
+//   walmart: {
+//     entry: "src/walmart/main.js",
+//     template: "src/walmart/walmart.htm",
+//     filename: "walmart.htm"
+//   }
+// },
